52ce8a616a1e37655b15dd17a73d1f69d13d112f
Yana Agun Siswanto <yana.developer@gmail.com>
2017-08-16 20:35:31 +0700

    Bug fix
    Fix staff cant access the url directly

--- plugin.rb
+++ plugin.rb
@@ -27,7 +27,7 @@ module ::DiscourseZendeskPlugin::Helper
 end
 
 Discourse::Application.routes.append do
-  get '/admin/plugins/zendesk-plugin' => 'admin/plugins#index', constraints: ::AdminConstraint.new
+  get '/admin/plugins/zendesk-plugin' => 'admin/plugins#index', constraints: ::StaffConstraint.new
   post '/zendesk-plugin/preferences' => 'discourse_zendesk_plugin/zendesk#preferences', constraints: ::StaffConstraint.new
   post '/zendesk-plugin/issues' => 'discourse_zendesk_plugin/issue#create', constraints: ::StaffConstraint.new
 end
