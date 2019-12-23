d4e0cba6a8a828e09c56c2b3afed44ee20ae060a
David Taylor <david@taylorhq.com>
2019-12-23 16:01:15 +0000

    DEV: Bump gem versions
    This should also stop the Faraday deprecation notices

--- plugin.rb
+++ plugin.rb
@@ -8,9 +8,9 @@
 # Require gems
 
 gem 'inflection', '1.0.0'
-gem 'mime-types-data', '3.2015.1120'
-gem 'mime-types', '3.1'
-gem 'zendesk_api', '1.14.4'
+gem 'mime-types-data', '3.2019.1009'
+gem 'mime-types', '3.3'
+gem 'zendesk_api', '1.24.0'
 
 enabled_site_setting :zendesk_enabled
 
